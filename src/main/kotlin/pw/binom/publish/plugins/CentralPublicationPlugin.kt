a6f166cb418783a62187aca137b1270d8faa80b2
subochev <caffeine.mgn@gmail.com>
2022-05-07 21:40:47 +0300

    refactoring

--- src/main/kotlin/pw/binom/publish/plugins/CentralPublicationPlugin.kt
+++ src/main/kotlin/pw/binom/publish/plugins/CentralPublicationPlugin.kt
@@ -6,19 +6,37 @@ import pw.binom.publish.*
 import java.net.URI
 import java.util.logging.Logger
 
-private const val msg = "Publication to Binom repository is disabled."
+private const val CENTRAL_USERNAME_PROPERTY = "binom.central.username"
+private const val CENTRAL_PASSWORD_PROPERTY = "binom.central.password"
+private const val msg = "Publication to Central repository is disabled."
 
 class CentralPublicationPlugin : Plugin<Project> {
     private val logger = Logger.getLogger(this::class.java.name)
     override fun apply(target: Project) {
         target.afterEvaluate {
-            val centralUserName = target.stringProperty("binom.central.username")
-            val centralPassword = target.stringProperty("binom.central.password")
             val publishing = target.publishing
             if (publishing == null) {
                 target.logger.warn("$msg $PUBLISH_PLUGIN_NOT_EXIST_MESSAGE")
                 return@afterEvaluate
             }
+
+            val centralUserName = target.propertyOrNull(CENTRAL_USERNAME_PROPERTY)
+            val centralPassword = target.propertyOrNull(CENTRAL_PASSWORD_PROPERTY)
+
+            if (centralUserName == null || centralPassword == null) {
+                val sb = StringBuilder()
+                sb.appendLine("$msg Reasons:")
+                if (centralUserName == null) {
+                    sb.appendLine("  $CENTRAL_USERNAME_PROPERTY not set")
+                }
+
+                if (centralPassword == null) {
+                    sb.appendLine("  $CENTRAL_PASSWORD_PROPERTY not set")
+                }
+                target.logger.warn(sb.toString())
+                return@afterEvaluate
+            }
+
             publishing.repositories {
                 it.maven {
                     it.name = "Central"
