3df65a79d13492af865121ef90ee13a28009bea7
subochev <caffeine.mgn@gmail.com>
2022-05-08 02:26:41 +0300

    refactoring

--- src/main/kotlin/pw/binom/publish/plugins/LintKotlinPlugin.kt
+++ src/main/kotlin/pw/binom/publish/plugins/LintKotlinPlugin.kt
@@ -2,6 +2,7 @@ package pw.binom.publish.plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jmailen.gradle.kotlinter.KotlinterExtension
 import pw.binom.publish.applyPluginIfNotApplied
 import pw.binom.publish.eachKotlinNativeCompile
 
@@ -9,6 +10,10 @@ class LintKotlinPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.applyPluginIfNotApplied(org.jmailen.gradle.kotlinter.KotlinterPlugin::class)
         val lintKotlin = target.tasks.findByName("lintKotlin")
+        val kotlinterExtension = target.extensions.findByType(KotlinterExtension::class.java)
+        kotlinterExtension?.also {
+            it.disabledRules = arrayOf("no-wildcard-imports")
+        }
         target.tasks.findByName("compileKotlin")?.dependsOn(lintKotlin)
         target.tasks.eachKotlinNativeCompile {
             it.dependsOn(lintKotlin)
